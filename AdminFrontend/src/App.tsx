a7f85ee7b4b457b247b4e9188190e5969b31d4b6
LKolinko <lskolinko@gmail.com>
2024-09-28 23:21:25 +0300

    React material ui template

---
+++ AdminFrontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from "@mui/material";
+
+const App = () => {
+  return (
+    <Button variant="outlined" size="large">Admin</Button>
+  );
+}
+
+export default App;
