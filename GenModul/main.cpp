33be183b91504dbd67bcf60953c58a997d5d06ad
LKolinko <lskolinko@gmail.com>
2024-09-28 22:51:09 +0300

    Httplib template

---
+++ GenModul/main.cpp
@@ -0,0 +1,11 @@
+#include "cpp-httplib/httplib.h"
+
+int main() {
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+}
